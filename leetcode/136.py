7b5e5ce7e8cfa69199b3cc2bc6d8b99e2e0ef1fe
Justin Casso <jcasso@agari.com>
2020-04-11 21:34:23 -0700

    Add solution  - Problem: 136  - Rating: Good  - Set: Leetcode

---
+++ leetcode/136.py
@@ -0,0 +1,22 @@
+#!/Library/Frameworks/Python.framework/Versions/3.8/bin/python3
+
+# Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+# Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+# Example 1:
+# Input: [2,2,1]
+# Output: 1
+
+# Example 2:
+# Input: [4,1,2,1,2]
+# Output: 4
+
+class Solution(object):
+    def singleNumber(self, nums):
+        pairless = set([])
+        for i in nums:
+            if i not in pairless:
+                pairless.add(i)
+            else:
+                pairless.remove(i)
+        return next(iter(pairless))
